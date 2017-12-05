d12c7c61caddece103c8a1ee5c8a054157b7e89a
Rodney R <majid.muhassan@gmail.com>
2017-12-05 21:10:58 +0000

    The logic now displays all the items for sale, id's, their names, prices and department

---
+++ bamazonCustomer.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    // connection.end();
+    afterConnection();
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
