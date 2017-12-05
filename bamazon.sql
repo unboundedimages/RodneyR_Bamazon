085c3ab37607f08fd4e9a92b1ea6a9be42dab59f
Rodney R <majid.muhassan@gmail.com>
2017-12-05 20:53:01 +0000

    I have the table with the mock data showing up on the php site.  The command that worked on the terminal to add the data was mysql -u root < bamazon.sql. The next step is to create the logic in bamazonCustomer.js.

---
+++ bamazon.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+id_item INTEGER(10) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR (100) NOT NULL,
+price INTEGER (10) NOT NUll,
+stock_quantity INTEGER (10) NOT NULL,
+PRIMARY KEY (id_item)
+);
+
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffee cans", 7.50, "foods", 3249);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffee mug", 11.50, "houseware", 6546);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffee maker", 59.50, "appliances", 816);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("vanilla creamer", 2.50, "foods", 23846);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("hazelnut creamer", 4.50, "foods", 35319);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffe filter", 1.99, "kitchen", 3636);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffee beans", 39.99, "foods", 2836);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffee grinder", 119.99, "appliances",674);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("coffee packs", .99, "foods", 47694);
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("industrial size cans", 79.99, "bulk",1246);
