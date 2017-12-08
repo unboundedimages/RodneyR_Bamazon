ebcb999bcdab30c65debd600a0e876078677b493
Rodney R <majid.muhassan@gmail.com>
2017-12-08 15:01:11 +0000

    add some comments explaining what function does what.  Everything works.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,32 +1,30 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  // Your username
   user: "root",
-  // Your password
   password: "",
   database: "bamazon_db"
 });
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  // connection.end();
   afterConnection();
-  // connection.end();
 });
-
+//after connection is established to the bamazon_db, a connection query is created inside this function
+//from the products table
 function afterConnection() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     numberTwo(res);
   });
-  // connection.end();
-  // afterConnection()
 }
-
+//number two  This fucntion has an argument passed in it called res that is pulled from prior function
+// The information stored in res will be used in the choices portion of this inquirer object.
+// The function in choices will loop through the array of results from the product table and pull
+// the item id, the product name, and the price associated with it.  When the loop is complete,
+// the information will be stored in blank array to be used later by using the push command.
 function numberTwo(res) {
   // afterConnection()
   inquirer.prompt([{
@@ -40,10 +38,13 @@ function numberTwo(res) {
             choiceArray.push(res[i].id_item + "  " + res[i].product_name + " $ " + res[i].price.toFixed(2))
           }
           return choiceArray;
-
         }
-
       },
+      //After the user selects the item they want to purchase, This object will prompt the user to input
+      //the quantity they want to buy.  A validate condtion is applied to make sure the user inputs a
+      //numeric number instead of alphanumeric.  This to make sure the user doesn't misspell werds.
+      //if a NaN or not  a number (letters) is input by the user, they will not be allowed to proceed
+      //until they provide a numeric value.
       {
         name: "quantity",
         type: "input",
@@ -58,23 +59,17 @@ function numberTwo(res) {
       }
     ])
     .then(function(answer) {
-      // afterConnection()
-
       connection.query(
         "SELECT * FROM products",
         function(err, res) {
           if (err) throw err;
-
           var dbItem;
-
-          // find the item with a matching name
           for (var i = 0; i < res.length; i++) {
             if (answer.items.indexOf(res[i].product_name) >= 0) {
               dbItem = res[i];
             }
-          }
-
-          // if we found a match
+          } //once confirmed match the answer with the database value  if out of stock
+          //alert user of insufficient stock and loop them back to choose something else.
           if (dbItem) {
             if (dbItem.stock_quantity < 0) {
               console.log("insufficent stock")
@@ -87,14 +82,11 @@ function numberTwo(res) {
               console.log("Your total is: $" + total.toFixed(2));
               updateProduct(newUnitVol, newPodQty);
             }
-
           }
         });
-
     });
 }
-
-// //next step is to update the units/stock quantity in the database.
+//update inventory
 function updateProduct(newUnitVol, newPodQty) {
   console.log("updating db");
   var query = connection.query("UPDATE products SET ? WHERE ?", [{
@@ -105,14 +97,11 @@ function updateProduct(newUnitVol, newPodQty) {
     }
   ], function(err, resp) {
     if (err) console.log(err);
-    // console.log(resp);
-
     buyORbye()
   });
 }
-
+//choose whether to continue buying or to exit the program
 function buyORbye() {
-  // console.log("would you like to purchase another item?");
   inquirer.prompt({
     name: "buyORbYe",
     type: "rawlist",
@@ -121,7 +110,11 @@ function buyORbye() {
   }).then(function(answer) {
     if (answer.buyORbYe.toUpperCase() === "BUY") {
       afterConnection()
-    };
+    }
+    else {
+      console.log("Thank for shopping.  Come back soon!")
+      connection.end()
+    }
 
   })
 }
